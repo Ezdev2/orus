b3a084b4e1a72ad063cd7df9cf406309b28c71eb
ezdev2 <fansoaezra@gmail.com>
2024-04-17 11:56:50 +0300

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sub.orussite.com",
-  base: "/orussite",
+  site: "https://ezdev2.github.io/orus/",
+  base: "/",
   integrations: [tailwind(), astroI18next()],
   vite: {
     ssr: {
