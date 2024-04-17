08c9b5e374ded5b822dfb79618a8dad04dc46588
ezdev2 <fansoaezra@gmail.com>
2024-04-17 11:36:43 +0300

    update repository

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://sub.orussite.com",
+  base: "/orussite",
+  integrations: [tailwind(), astroI18next()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
